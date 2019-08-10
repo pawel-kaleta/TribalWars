e78ffce8838eb0ac1a6a4a61b30d6e8461bd807f
pawel-kaleta <43814001+pawel-kaleta@users.noreply.github.com>
2019-08-10 11:11:07 +0200

    Update Cofki_z_okna_informacji_o_wiosce.js

--- Cofki_z_okna_informacji_o_wiosce.js
+++ Cofki_z_okna_informacji_o_wiosce.js
@@ -18,41 +18,47 @@ if (document.URL.indexOf("screen=info_village") == -1) {
     UI.InfoMessage('Przejdź do przeglądu informacji o wiosce, aby użyć tego skryptu!', 5000, 'error');
 }
 else {
-    if ($('#content_value > div > h3')[0].innerText == "Obrona") {
-        let units = game_data.units.filter(u => u !== 'militia');
+    let noStationingTroops = true;
 
-        let tables = $('table.vis');
-        let table = tables[tables.length - 1];
+    if ($('#content_value > div > h3').length > 0) {
+        if ($('#content_value > div > h3')[0].innerText == "Obrona") {
+            let units = game_data.units.filter(u => u !== 'militia');
 
-        for (let i = 2; i < table.rows.length - 1; i++) {
-            if (table.rows[i].cells[14].children[0].innerText == "Cofnij") {
-                let url = table.rows[i].cells[14].children[0].href;
-                url = url.replace("action=all_back", "action=back");
+            let tables = $('table .vis');
+            let table = tables[tables.length - 1];
 
-                console.log(url);
-                for (let j = 0; j < units.length; j++) {
-                    let cell$ = $(table.rows[i].cells[j + 1]);
-                    let count = Number(cell$.text());
-                    if (count !== 0) {
-                        console.log(count);
-                        cell$.css('cursor', 'pointer');
-                        cell$.css('text-decoration', 'underline');
-                        let form_data = {};
-                        form_data[units[j]] = count;
-                        cell$.on('click', function () {
-                            cell$.off('click');
-                            $.post(url, form_data, (data, status, xhr) => {
-                                console.log(status);
-                                console.log(xhr);
-                                cell$.css('cursor', '');
-                                cell$.css('text-decoration', '');
-                                cell$.text('0');
-                                cell$.addClass('hidden');
-                            });
-                        });
+            for (let i = 1; i < table.rows.length; i++) {
+                if (table.rows[i].cells[0].innerText != "Z tej wioski:") {
+                    if (table.rows[i].cells[14].children[0].innerText === "Cofnij") {
+                        let url = table.rows[i].cells[14].children[0].href;
+                        url = url.replace("action=all_back", "action=back");
+                        for (let j = 0; j < units.length; j++) {
+                            let cell$ = $(table.rows[i].cells[j + 1]);
+                            let count = Number(cell$.text());
+                            if (count !== 0) {
+                                noStationingTroops = false;
+                                cell$.css('cursor', 'pointer');
+                                cell$.css('text-decoration', 'underline');
+                                let form_data = {};
+                                form_data[units[j]] = count;
+                                cell$.on('click', function () {
+                                    cell$.off('click');
+                                    $.post(url, form_data, (data, status, xhr) => {
+                                        cell$.css('cursor', '');
+                                        cell$.css('text-decoration', '');
+                                        cell$.text('0');
+                                        cell$.addClass('hidden');
+                                    });
+                                });
+                            }
+                        }
                     }
                 }
-            }  
+            }
         }
+    }
+
+    if (noStationingTroops == true) {
+            UI.InfoMessage('Zdaje się, że nie ma czego wycofać.', 5000);
     }
 }
