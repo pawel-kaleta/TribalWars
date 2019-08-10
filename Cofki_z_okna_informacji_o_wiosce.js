cd4fcf88b2b6277ee67b9eab44390060e3c5c4cc
pawel-kaleta <43814001+pawel-kaleta@users.noreply.github.com>
2019-08-10 10:09:05 +0200

    Update Cofki_z_okna_informacji_o_wiosce.js

--- Cofki_z_okna_informacji_o_wiosce.js
+++ Cofki_z_okna_informacji_o_wiosce.js
@@ -1,1 +1,62 @@
+javascript:
+
+//	author:		PabloCanaletto
+//	version:	1.0
+//	history of changes:
+//		1.0 - 10.08.2019 by PabloCanaletto
+//			first release
+//					
+//	disclaimer:	You are free to use this script in any way you like and to submit your own changes.
+//				I would only appreciate you to leave this notification about my orginal authorship untouched
+//				with the whole history of changes.
+
+// FUNCTIONALITY
+// This script allows to withdraw units by type from village. To use on village_info tab.
+
+
+if (document.URL.indexOf("screen=info_village") == -1) {
+    UI.InfoMessage('Przejdź do przeglądu informacji o wiosce, aby użyć tego skryptu!', 5000, 'error');
+}
+else {
+    if ($('#content_value > div > h3')[0].innerText == "Obrona") {
+        let units = game_data.units.filter(u => u !== 'militia');
+
+        let tables = $('table.vis');
+        let table = tables[tables.length - 1];
+
+        for (let i = 2; i < table.rows.length - 1; i++) {
+            if (table.rows[i].cells[14].children[0].innerText == "Cofnij") {
+                let url = table.rows[i].cells[14].children[0].href;
+                url = url.replace("action=all_back", "action=back");
+
+                console.log(url);
+                for (let j = 0; j < units.length; j++) {
+                    let cell$ = $(table.rows[i].cells[j + 1]);
+                    let count = Number(cell$.text());
+                    if (count !== 0) {
+                        console.log(count);
+                        cell$.css('cursor', 'pointer');
+                        cell$.css('text-decoration', 'underline');
+                        let form_data = {};
+                        form_data[units[j]] = count;
+                        cell$.on('click', function () {
+                            cell$.off('click');
+                            $.post(url, form_data, (data, status, xhr) => {
+                                console.log(status);
+                                console.log(xhr);
+                                cell$.css('cursor', '');
+                                cell$.css('text-decoration', '');
+                                cell$.text('0');
+                                cell$.addClass('hidden');
+                            });
+                        });
+                    }
+                }
+            }  
+        }
+    }
+}
+
+
+
 
