2a4cb0abcd89513fd0bcf0b14ae0286ef845af8d
pawel-kaleta <43814001+pawel-kaleta@users.noreply.github.com>
2021-02-02 20:22:46 +0100

    Update Dysponent_Surowcowy_sygnatura.js

--- Dysponent_Surowcowy_sygnatura.js
+++ Dysponent_Surowcowy_sygnatura.js
@@ -1,15 +1,14 @@
 javascript:
 
 var DysponentSurowcowy = {
-	// USTAWIENIA DOMYSLNE
-	resourcesFillTo: [20000, 25000, 25000],     // wypelniaj do tej wartosci
-	resourcesSafeguard: [2000, 2000, 2000],     // zabezpieczenie w surowcach, wypelniane priorytetowo
-	tradersSafeguard: 0,                        // zabezpieczenie w kupcach
-	considerOngoingTransports: true,			// uwzglednij przychodzace transporty (tak - true, nie - false)
-	overFlowThreshold: 75,						// % pojemnosci spichlerza, powyzej ktorego zapobiegaj przelewaniu sie
-	extendedOptimization: true,					// czy optymalizacja moze generowac dodatkowych odbiorcow (wiecej klikania)
-	minSummon: 0,								// minimalne wezwanie
-	debug: true
+    // USTAWIENIA DOMYSLNE
+    resourcesFillTo: [75000, 75000, 75000],    // wypełniaj do tej wartości
+    resourcesSafeguard: [20000, 20000, 20000], // zabezpieczenie w surowcach, wypełniane priorytetowo
+    tradersSafeguard: 0,                       // zabezpieczenie w kupcach
+    considerOngoingTransports: true,           // uwzględnij przychodzące transporty (tak - true, nie - false)
+    overFlowThreshold: 75,                     // % pojemności spichlerza, powyżej ktorego zapobiegaj przelewaniu się
+    extendedOptimization: true,                // czy optymalizacja może generowac dodatkowych odbiorców (wiecej klikania)
+    minSummon: 0                               // minimalne wezwanie
 };
 
 $.getScript('https://media.innogamescdn.com/com_DS_PL/skrypty/Dysponent_Surowcowy.js');
